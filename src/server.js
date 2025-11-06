7c46cf5e5f462f12dfcee45684ca26d665de39a4
ItsMeYash777 <quarkspubg2@gmail.com>
2025-11-06 17:29:31 +0530

    added the cors config

--- src/server.js
+++ src/server.js
@@ -3,7 +3,13 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(cors());
+app.use(cors({
+  origin: [ process.env.FRONTEND_URL, // though added the cors config, in production this will be useful to restrict the access to the api to only the frontend url
+  'http://localhost:3000'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const categoryRoutes = require('./routes/categoryRoutes');
